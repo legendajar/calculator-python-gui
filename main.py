b1e033370649de8a7063cace94eac7c29e8ecd3b
legendajar <harshsolanki9898@gmail.com>
2023-07-04 18:57:20 +0530

    Commit

--- main.py
+++ main.py
@@ -27,16 +27,11 @@ def clear_field():
     text_result.delete(1.0,"end")
 
 
-
-
-
 root = tk.Tk()
 root.geometry("300x275")
 
 text_result = tk.Text(root, height = 2, width=16, font=("Arial", 24))
 text_result.grid(columnspan=5)
-text_result.pack(fill=X,ipadx=8,ipady=10,padx=10)
-
 
 btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1),width=5,font=("Arial", 14))
 btn_1.grid(row = 2, column = 1)
